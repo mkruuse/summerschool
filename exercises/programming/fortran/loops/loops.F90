82fc0bf0ef60a8a523bb841e34996b8c5234910e
maarjakruuse <maarjakruuse@gmail.com>
2017-06-28 11:10:14 +0300

    Changed the code

--- exercises/programming/fortran/loops/loops.F90
+++ exercises/programming/fortran/loops/loops.F90
@@ -1,20 +1,24 @@
 program loops
   implicit none
   ! TODO define parameters nx and ny
-  integer, parameter :: nx=10, ny=10
+  integer, parameter :: nx=10, ny=10, k=11, l=11
   real :: matrix(0:nx+1, 0:ny+1)
   ! TODO: define real-valued array A
   integer :: i, j
- matrix=65.0 
- do i=0, nx
-   matrix(i,0)=20.0
-   matrix(i,ny)=70.0
-  end do
-  do j=0, ny
-   matrix(0,j)=85.0
-   matrix(i,j)=5.0
-  end do
- !write (*,*) 'matrix', matrix 
+ matrix=65.0
+ matrix(1:k,0)=20.0
+ matrix(1:k,l)=70
+ matrix(0,1:l)=85.0
+ matrix(k,1:l)=5.0
+! do i=0, k
+!  matrix(i,0)=20.0
+!  matrix(i,l)=70.0
+! end do
+! do j=0, l
+!  matrix(0,j)=85.0
+!  matrix(k,j)=5.0
+! end do
+!write (*,*) 'matrix', matrix 
   ! TODO initialize array A here
 
 
