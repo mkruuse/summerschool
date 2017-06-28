7fcfd51b5c21ea53aca68d4ad38a9d9ac3bae9b8
maarjakruuse <maarjakruuse@gmail.com>
2017-06-28 10:34:28 +0300

    exercise 1

--- exercises/programming/fortran/hello/hello.F90
+++ exercises/programming/fortran/hello/hello.F90
@@ -1,5 +1,17 @@
 program hello
   implicit none
-
-  write (*,*) 'Hello world from Fortran!'
+!  integer, parameter :: nx=10, ny=10
+  real :: a,b,c
+  real :: sum
+ ! real :: matrix(0:nx,0:ny)
+  write(*,*) 'Write sides for a triangle'
+  read(*,*) a,b,c
+  if (a>0 .and. b >0 .and. c>0) then
+   sum=a+b+c
+   write (*,*) 'Triangle perimeter', sum
+  else
+    write (*,*) 'Negative values'
+    stop
+  end if
+! write (*,*) 'Hello world from Fortran!'
 end program hello
