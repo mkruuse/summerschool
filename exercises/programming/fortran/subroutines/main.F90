7213c33b26bfe60eb22c30ac064dbb98f35ce365
maarjakruuse <maarjakruuse@gmail.com>
2017-06-28 19:54:40 +0300

    Main file for assignment 6

--- exercises/programming/fortran/subroutines/main.F90
+++ exercises/programming/fortran/subroutines/main.F90
@@ -3,22 +3,25 @@ program subroutines
   implicit none
 ! TODO: define the arrays
   integer :: nx, ny
-
+  real, dimension(:,:), allocatable :: previous, current
   write (*,*) 'Give number of rows and columns for matrix A:'
   read (*,*) nx, ny
 
-  allocate(previous(nx,ny), current(nx,ny))
-
   ! initialize the array
-  call initialize(previous)
+  call initialize(nx, ny, previous)
+  call initialize(nx, ny, current)
 
-  call write_field(previous)
+  call write_field(previous, nx, ny)
 
   ! compute the Laplacian
-  call laplacian(current, previous)
+  call laplacian(current, previous, nx, ny)
 
   ! print the result array
-  call write_field(current)
- 
+  call write_field(current, nx, ny)
+  
+  deallocate(previous)
+!  call finalize(previous)
+  deallocate(current)
+!  call finalize(previous) 
 end program subroutines
 
