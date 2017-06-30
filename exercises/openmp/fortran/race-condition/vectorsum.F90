1a701cf8ec82412738e97eb5afd428abea7ad33e
mkruuse <maarjakruuse@gmail.com>
2017-06-30 12:11:29 +0300

    Assignment 4 openMPI

--- exercises/openmp/fortran/race-condition/vectorsum.F90
+++ exercises/openmp/fortran/race-condition/vectorsum.F90
@@ -1,6 +1,8 @@
 program vectorsum
+  use omp_lib
   use iso_fortran_env, only: int64
   implicit none
+  real :: starttime, endtime
   integer, parameter :: ik = int64
   integer(kind=ik), parameter :: nx = 102400_ik
 
@@ -15,8 +17,14 @@ program vectorsum
 
   sum = 0
   ! TODO: Parallelize the computation
+  starttime=omp_get_wtime()
+  print*, "START:", starttime
+  !$omp parallel do shared(vecA) reduction(+:sum)
   do i = 1, nx
      sum = sum + vecA(i)
   end do
+  !$omp end parallel do
+  endtime=omp_get_wtime()
+  print*, "END", endtime
   write(*,*) 'Sum: ', sum
 end program vectorsum
