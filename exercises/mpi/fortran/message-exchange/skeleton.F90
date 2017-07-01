12283bd5d144ddda0990f7551aae9d2991535699
mkruuse <maarjakruuse@gmail.com>
2017-07-01 16:27:35 +0300

    Message exchange assign 2

--- exercises/mpi/fortran/message-exchange/skeleton.F90
+++ exercises/mpi/fortran/message-exchange/skeleton.F90
@@ -1,7 +1,7 @@
 program exchange
   use mpi
   implicit none
-  integer, parameter :: size = 100
+  integer, parameter :: size = 100000
   integer :: rc, myid, ntasks, count
   integer :: status(MPI_STATUS_SIZE)
   integer :: message(size)
@@ -15,11 +15,13 @@ program exchange
 
   ! TODO: Implement sending and receiving as defined in the assignment
   if ( myid == 0 ) then
-
+     call mpi_send(message, size, mpi_integer, myid+1, 10, MPI_COMM_WORLD, rc)
+     call mpi_recv(receiveBuffer, size, mpi_integer, myid+1, MPI_ANY_TAG, MPI_COMM_WORLD, status, rc)  
      write(*,'(A10,I3,A10,I3)') 'Rank: ', myid, &
           ' received ', receiveBuffer(1)
   else if (myid == 1) then
-
+     call mpi_recv(receiveBuffer, size, mpi_integer, myid-1, MPI_ANY_TAG, MPI_COMM_WORLD, status, rc)
+     call mpi_send(message, size, mpi_integer, myid-1, 11, MPI_COMM_WORLD,rc)
      write(*,'(A10,I3,A10,I3)') 'Rank: ', myid, &
           ' received ', receiveBuffer(1)
   end if
