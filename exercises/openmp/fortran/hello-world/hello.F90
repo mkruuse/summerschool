44d2254b4934bdf4c18c7a5b163e330eecb3c205
mkruuse <maarjakruuse@gmail.com>
2017-07-02 16:53:41 +0300

    Hello world assignment

--- exercises/openmp/fortran/hello-world/hello.F90
+++ exercises/openmp/fortran/hello-world/hello.F90
@@ -1,9 +1,20 @@
 program hello
+  use omp_lib
   implicit none
-
-  print *, 'Hello world!'
-  !$omp parallel
+  integer :: active_threads
+  integer :: thread_num
+  
+  print*, "Hello World!"
+  !$OMP PARALLEL SHARED(thread_num) PRIVATE(active_threads)
+  thread_num=omp_get_thread_num()
+  !$OMP SINGLE
+  active_threads=omp_get_num_threads()
+  !$OMP END SINGLE
+  !$OMP CRITICAL 
+  print*, "The thread number:", thread_num
+  !$OMP END CRITICAL
   print *, 'X'
-  !$omp end parallel
+  !$OMP END PARALLEL
+  print*, "The number of threads", active_threads
 
 end program hello
