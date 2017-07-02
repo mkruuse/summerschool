3316de0275a57fc54fec2efab8b1957281f88045
mkruuse <maarjakruuse@gmail.com>
2017-07-02 16:51:40 +0300

    Non-blocking communication

--- exercises/mpi/fortran/message-chain/skeleton.F90
+++ exercises/mpi/fortran/message-chain/skeleton.F90
@@ -13,7 +13,7 @@ program basic
 
   call mpi_init(rc)
   call mpi_comm_rank(MPI_COMM_WORLD, myid, rc)
-  call mpi_comm_sizeMPI_COMM_WORLD, ntasks, rc)
+  call mpi_comm_size(MPI_COMM_WORLD, ntasks, rc)
 
   message = myid
 
@@ -23,14 +23,15 @@ program basic
 
   ! TODO: Send and receive as defined in the assignment
   if ( myid < ntasks-1 ) then
-
+     call  mpi_send(message, size, mpi_integer, myid+1, myid+1, MPI_COMM_WORLD, rc)
+     call  mpi_recv(receiveBuffer, size, mpi_integer, myid+1, myid, MPI_COMM_WORLD, status, rc)
      write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
           ' Sent elements: ',size, &
           '. Tag: ', myid+1, '. Receiver: ', myid+1
   end if
-
   if ( myid > 0 ) then
-
+     call mpi_recv(receiveBuffer, size, mpi_integer, myid-1, myid, MPI_COMM_WORLD, status, rc)
+     call mpi_send(message, size, mpi_integer, myid-1, myid-1, MPI_COMM_WORLD, rc)
      write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
           ' First element: ', receiveBuffer(1)
   end if
