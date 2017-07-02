00e676e2e148a5a2a05df587ffa789af969cc6dc
mkruuse <maarjakruuse@gmail.com>
2017-07-02 16:52:28 +0300

    Non-blocking communication add

--- exercises/mpi/fortran/message-chain/skeletone_assign3.F90
+++ exercises/mpi/fortran/message-chain/skeletone_assign3.F90
@@ -10,13 +10,14 @@ program basic
   integer :: receiveBuffer(size)
   integer :: source, destination
   real(REAL64) :: t0, t1
-  
+  integer, dimension(2) :: variable_request
+  integer :: array_of_statuses(mpi_status_size,2)
   call mpi_init(rc)
   call mpi_comm_rank(MPI_COMM_WORLD, myid, rc)
   call mpi_comm_size(MPI_COMM_WORLD, ntasks, rc)
   
   message = myid
-  ! Set source and destination ranks
+ 
   if (myid < ntasks-1) then
      destination = myid + 1
   else
@@ -32,25 +33,27 @@ program basic
   call mpi_barrier(mpi_comm_world, rc)
   t0 = mpi_wtime()
  ! TODO: Send and receive as defined in the assignment
- !  if ( myid < ntasks-1 ) then
- !     call  mpi_sendrecv(message, size, mpi_integer, source, 0, receiveBuffer, size, mpi_integer, destination, 1,  MPI_COMM_WORLD, status, rc) 
- !     write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
- !          ' Sent elements: ',size, &
- !          '. Tag: ', myid+1, '. Receiver: ', myid+1, 'The status: ', status 
- !   end if
- !  if ( myid > 0 ) then
- !     call mpi_sendrecv(message, size, mpi_integer, source, 1, receiveBuffer, size, mpi_integer, destination,0, MPI_COMM_WORLD, status, rc)
- !     write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
- !          ' First element: ', receiveBuffer(1), 'The status: ', status
- !  end if
- 
-  call mpi_sendrecv(message, size, MPI_INTEGER, destination, myid + 1, receiveBuffer, size, MPI_INTEGER, source, MPI_ANY_TAG, &
-       MPI_COMM_WORLD, status, rc)
+!3. Message chain:
+!  call mpi_sendrecv(message, size, MPI_INTEGER, destination, myid + 1, receiveBuffer, size, MPI_INTEGER, source, MPI_ANY_TAG, &
+!       MPI_COMM_WORLD, status, rc)
+!  print*, "Message chain!"
+!  write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
+!       ' Sent elements: ', size, &
+!       '. Tag: ', myid + 1, &
+!       '. Receiver: ', destination
+
+!7. Collective operators:
+
+  call mpi_Irecv(receiveBuffer, size, mpi_integer, source, MPI_ANY_TAG, MPI_COMM_WORLD, variable_request(1), rc)
+  call mpi_Isend(message, size, mpi_integer, destination, myid+1, MPI_COMM_WORLD, variable_request(2), rc)    
+  
+  call mpi_waitall(2,variable_request, array_of_statuses, rc) !MPI_STATUSES_IGNORE 
+
+  print*, "Collective operators!" 
   write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
        ' Sent elements: ', size, &
        '. Tag: ', myid + 1, &
-       '. Receiver: ', destination
-
+       '. Receiver: ', destination    
   ! Finalize measuring the time and print it out
   t1 = mpi_wtime()
   call mpi_barrier(mpi_comm_world, rc)
