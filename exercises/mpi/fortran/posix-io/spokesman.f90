ce97ff6e5ecac69a4bc656c34044b41969afd409
mkruuse <maarjakruuse@gmail.com>
2017-07-03 20:28:35 +0300

    MPI-O ssign. 12

--- exercises/mpi/fortran/posix-io/spokesman.f90
+++ exercises/mpi/fortran/posix-io/spokesman.f90
@@ -7,7 +7,9 @@ program pario
   integer :: rc, my_id, ntasks, localsize, i
   integer, dimension(:), allocatable :: localvector
   integer, dimension(datasize) :: fullvector
-
+  integer :: funit=11
+  character(10) :: fname="out.dat"
+  
   call mpi_init(rc)
   call mpi_comm_size(mpi_comm_world, ntasks, rc)
   call mpi_comm_rank(mpi_comm_world, my_id, rc)
@@ -27,19 +29,48 @@ program pario
 
   localvector = [(i + my_id * localsize, i=1,localsize)]
 
-  call single_writer()
 
   deallocate(localvector)
+  funit=11
+
+  call single_writer()
+  call reader()
   call mpi_finalize(rc)
 
 contains
 
+!12.a)
   subroutine single_writer()
     implicit none
-
     ! TODO: Implement a function that writers the whole array of elements
-    !       to a file so that single process is responsible for the file io
+    !       to a file so that single process is responsible for the file io 
+   call mpi_gather(localvector,localsize, MPI_INTEGER, fullvector, localsize, MPI_INTEGER, 0, MPI_COMM_WORLD, rc )
 
+   if(my_id==writer_id) then !writer_id=0
+    open(funit, file="fname", form="unformatted", status="replace", access="stream")
+    write(funit) fullvector
+    close(funit)   
+   end if
   end subroutine single_writer
+  
+!12.b)
+  subroutine reader()
+  implicit none
+  
+  if (my_id==writer_id) then
+   open(funit, file="fname", form="unformatted", status="old", access="stream")
+   read(funit, pos=1) fullvector !pos=1, position where to start the stream
+   close(funit)
+   write(output_unit, "(A, IO, A)") "Read", size(fullvector)
+  end if
+    call mpi_scatter(fullvector, localsize, mpi_integer, localvector, &
+         & localsize, mpi_integer, writer_id, mpi_comm_world, rc)
 
+  end subroutine reader
+
+!13.
+subroutine writer()
+implicit none
+ call mpi_file_write_at()
+end subroutine reader
 end program pario
